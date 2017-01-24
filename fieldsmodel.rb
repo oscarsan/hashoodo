c067a9d08dd768170a9a2dd5ca9f178442457728
oscar sanhueza <oscar@hashdot.fi>
2017-01-24 22:13:03 +0200

    some changes in models, add fieldsmodel
    Signed-off-by: oscar sanhueza <oscar@hashdot.fi>

--- fieldsmodel.rb
+++ fieldsmodel.rb
@@ -21,13 +21,13 @@ models = XMLRPC::Client.new2("#{url}/xmlrpc/2/object").proxy
 #get all models ids
 ids = models.execute_kw(db, uid, password,
     'ir.model.fields', 'search',
-    [[]])
+    [[['name', '=', 'x_test']]])
 
-puts "#{ids.size} it was"
+puts "#{ids} it was"
 
 #get all product names
 fields = models.execute_kw(db, uid, password,
     'ir.model.fields', 'read',
     [ids], {'fields': ['model', 'name']})
 
-#puts fields
+puts fields
--- models.rb
+++ models.rb
@@ -44,14 +44,43 @@ puts models.execute_kw(db, uid, password,
     'ir.model', 'read',
     [ids])
 
+
+#get id of product.template model
+idsptemplate = models.execute_kw(db, uid, password,
+    'ir.model', 'search',
+    [[['model', '=', 'product.template']]])
+
+puts idsptemplate
+
+#show the model product.template
+producttemplate =  models.execute_kw(db, uid, password,
+    'ir.model', 'read',
+    [idsptemplate])
+
+puts producttemplate
+
 option = gets
-#add field to model
-puts  models.execute_kw(
-        db, uid, password,
-        'ir.model.fields', 'create', [{
-            model_id: ids.first, #this is the id of the produt.product model
-            name: "x_test",
-            ttype: "char",
-            state: "manual",
-            required: false
-        }])
+
+puts producttemplate[0]["id"]
+
+option = gets
+#get all fields from product.template names
+fields = models.execute_kw(db, uid, password,
+    'ir.model.fields', 'read',
+    [producttemplate[0]["field_id"]], {'fields': ['model', 'name']})
+
+puts fields
+
+
+
+#add field to model, at the moment this is a bit of a problem as you can create
+#infinite times this field in the model
+#puts  models.execute_kw(
+#        db, uid, password,
+#        'ir.model.fields', 'create', [{
+#            model_id: idsptemplate.first, #this is the id of the produt.product model
+#            name: "x_test",
+#            ttype: "char",
+#            state: "manual",
+#            required: false
+#        }])
